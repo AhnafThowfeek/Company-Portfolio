60ebb101ec59ef71f690a55c9a2256f418750f3a
aselapasinduD <asela.pasindu.dias17@gmail.com>
2024-09-26 07:10:23 +0530

    Stage: Development | Area: Home Page -> Hero Section | Work: Fix The Top Margin Bug

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import logo from "../assets/logo.jpg";
 
 const Home = () => {
+
+  const slogan1 = "Innovating Today".toUpperCase();
+  const slogan2 = "for a Brighter Tomorrow".toUpperCase();
+
   return (
     <div className='bg-neutral-100' id='home'>
         <div className='px-4 lg:px-14 max-w-screen-2xl mx-auto min-h-screen h-screen'>
-            <div className='my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-between gap-12'>
+            <div className='my-28 py-12 flex flex-col md:flex-row-reverse items-center justify-between gap-12'>
                 <div>
                   <img src={logo}/>
                 </div>
 
                 <div className='md:w-1/2'>
-                  <h1 className='text-5xl font-semibold mb-4 text-neutral-900 md:w-3/4 leading-sung'>INNENTA <span className='text-sky-600 leading-sung'><br/>SOLUTIONS</span></h1>
+                  <h1 className='text-5xl font-semibold mb-4 text-neutral-900 md:w-4/4 leading-sung'>{slogan1}<span className='text-sky-600 leading-sung'><br/>{slogan2}</span></h1>
                   <p className='text-neutral-400 text-base mb-8'>We believe that behind every successful business is a strong technological foundation. At Innenta Solutions, we partner with businesses to deliver innovative software that bridges the gap between their vision and digital reality, helping them unlock new opportunities and drive sustainable growth. </p>
                   <button className='btn-primary'>Get In Touch</button>
                 </div>
